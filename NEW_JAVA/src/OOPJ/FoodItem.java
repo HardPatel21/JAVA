3621ec76f226c5c524b62001236491d394ae7bcc
Hard Patel <hardp6432@gmail.com>
2025-05-13 11:14:25 +0530

    task done!

--- NEW_JAVA/src/OOPJ/FoodItem.java
+++ NEW_JAVA/src/OOPJ/FoodItem.java
@@ -1,4 +1,34 @@
 package OOPJ;
 
-public class FoodItem {
+class Food {
+    double price;
+    String itemName;
+    static String category;
+
+    public Food(String itemName, double price) {
+        this.itemName = itemName;
+        this.price = price;
+    }
+
+    static {
+        category = "beverages";
+    }
+
+    @Override
+    public String toString() {
+        return "Food{" +
+                "price=" + price +
+                ", itemName='" + itemName + '\'' +
+                '}';
+    }
 }
+
+public class FoodItem {
+    public static void main(String[] args) {
+        Food f = new Food("Lemon Juice", 35.90);
+        System.out.println(f);
+        System.out.println("Category :" + Food.category);
+    }
+}
+
+
--- NEW_JAVA/src/OOPJ/Static_KW.java
+++ NEW_JAVA/src/OOPJ/Static_KW.java
@@ -1,4 +1,34 @@
 package OOPJ;
 
-public class Static_KW {
+class Student_1 {
+    int rollNo;
+    String name;
+    static String collegeName;
+
+    static {
+        // static block: initialize all the static items here
+        System.out.println("--- static block called ---");
+        collegeName = "Govt. Engineering College";
+    }
+
+    public Student_1(int rollNo, String name) {
+        System.out.println("--- constructor called ---");
+        this.rollNo = rollNo;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "rollNo=" + rollNo + ", name='" + name + '\'' + '}';
+    }
 }
+
+public class Static_KW {
+    public static void main(String[] args) {
+        Student_1 s = new Student_1(420, "mohaklal");
+        //        System.out.println(s);
+        System.out.println("College Name: " + Student_1.collegeName);
+
+    }
+}
+
